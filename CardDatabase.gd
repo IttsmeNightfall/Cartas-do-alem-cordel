fb38c372a82cbfddb9940189b585c3c980d5ddfa
Igor Gabriel Rocha de Andrade <20221041110001@ifrn.local>
2025-12-17 10:58:58 -0300

    Adiciona database de cards e pasta assets

--- CardDatabase.gd
+++ CardDatabase.gd
@@ -4,7 +4,37 @@
 enum { ATTACK, HEALTH } # Facilita a leitura dos índices
 
 const CARDS = {
-	"Wolf": [4, 5], # [Ataque, Vida]
-	"Archer": [2, 3],
-	"Demon": [6, 2]
+    "Cachaceiro": [2, 4],
+    "Briguento": [2, 1],
+    "Matilha": [1, 2],
+    "Zeca-Peito-de-Ferro": [1, 7],
+    "Velho do Saco": [2, 4],
+    "Lambizói": [3, 3],
+    "O Sanfoneiro": [0, 6],
+    "Terezinha": [2, 5],
+    "Coronel Bezerra": [3, 6],
+    "Vaqueiro Jacó": [7, 7],
+    "Capitão-Mor": [3, 7],
+    "Cabra da Peste": [3, 6],
+    "Boto-Cor-de-Rosa": [2, 5],
+    "Besta Fera": [10, 10],
+    "Chupa Cabra": [6, 6],
+    "Pisadeira": [2, 5],
+    "Cabra-Cabriola": [2, 5],
+    "Curupira": [4, 4],
+    "Encourado": [4, 6],
+    "Matinta-Perera": [3, 3],
+    "Mula sem Cabeça": [2, 5],
+    "Corpo-Seco": [4, 1],
+    "Malassombrado": [3, 3],
+    "Cumade-Fulozinha": [1, 4],
+    "Romãozinho": [3, 3],
+    "Maçone": [4, 7],
+    "O Diabo Loiro": [8, 6],
+    "Balão": [2, 4],
+    "Zé Baiano": [3, 3],
+    "Sinhô Pereira": [3, 5],
+    "Jesuíno Brilhante": [2, 7],
+    "Manoel Elétrico": [3, 9],
+    "Mergulhão": [4, 4]
 }
